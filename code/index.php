efbab8cf430ee5b102767c9d55b9f2149f09b37d
wusong <877183659@qq.com>
2020-02-15 16:29:38 +0800

    docker-compose 增加nginx mysql redis php

---
+++ code/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$dsn = 'mysql:host=db;port=3306'; // host=db,这里的db其实是mysql的服务名
+$user = 'root';
+$password = 'root';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    print_r($dbh);
+} catch (PDOException $e) {
+    echo 'Connection failed: '.$e->getMessage();
+}
+$redis = new Redis();
+$redis->pconnect('redis', 6379);
+
+$key = 'first';
+$redis->incr($key);
+
+echo '页面浏览了：'.$redis->get($key); 
+die;
+echo phpinfo();
